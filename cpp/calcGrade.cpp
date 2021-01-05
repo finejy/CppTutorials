ee90117e14bd3adcb7ca5d6fd3f2f5043f7e04fc
finejy <yoon2926@khu.ac.kr>
2021-01-05 23:43:11 +0900

    I study about finishing while function with line change!

--- cpp/calcGrade.cpp
+++ cpp/calcGrade.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+	
 using namespace std;
 
 int main(){
@@ -22,10 +22,18 @@ int main(){
 	vector<double> homework;
 	double x;
 	
-	while (cin >> x)
+	while (getline(cin, x)){		
+		homework.push_back(x);	
+	}
+	/*이 부분은 개선이 필요할듯. 이렇게 되면 double 타입 이외의 입력이 들어와야 입력이 끝남.(공백과 줄바꿈으로는 종료되지 않음) 
+	  가장 깔끔한건 엔터 입력시 입력이 종료되는 것.*/
+	  
+	/*while (getline(cin, x){
 		homework.push_back(x);
-	/*이 부분은 개선이 필요할듯. 이렇게 되면 double 타입 이외의 입력이 들어와야 입력이 끝남.
-	  가장 깔끔한건 엔터 입력시 입력이 종료되는 것.*/ 
+	}
+	이 경우 x가 double 타입이라는 문제가 발생. 이때문에 x를 string타입으로 정의, push_back 전에 int로 변환할 경우 연산이 많아진다는 문제 발생.
+	## getline의 문제점. '공백'을 포함한 데이터를 입력받는 것이므로 string일 수 밖에 없음... 
+	*/ 
 	
 	typedef vector<double>::size_type vec_sz;
 	vec_sz size = homework.size();
