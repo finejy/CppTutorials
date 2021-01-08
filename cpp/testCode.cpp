38c82dac2605a4340b34f6599fda796b61dbdd31
finejy <yoon2926@khu.ac.kr>
2021-01-08 18:50:16 +0900

    New Update of testCode and Readme

--- cpp/testCode.cpp
+++ cpp/testCode.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+ 
 using namespace std;
 
 int main(){
+	/*문자열에 특수 기호 입력하는 법 
 	cout << "This (\") is a quote, and this (\\) is a backslash." << ends;
+	*/
+	/*size() 함수의 결과값이 index로 나오는지 실제 숫자로 나오는지 확인 
+	string s = "abcde";
+	string::size_type len = s.size();
+	cout << len << endl;
+	*/
 	return 0;
 }
