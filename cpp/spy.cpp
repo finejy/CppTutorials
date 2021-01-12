599cdfe1d7a0360d5dfcb8c3a0b8622e859dbf85
finejy <yoon2926@khu.ac.kr>
2021-01-12 23:23:53 +0900

    New question about combination.

---
+++ cpp/spy.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    vector<string> cloth_kind;
+    vector<int> kind_num;
+    vector<vector<string> >::size_type len = clothes.size();
+	
+	for(int i=0; i!=len; ++i){
+		if(find(cloth_kind.begin(), cloth_kind.end(), clothes[i][1]) != cloth_kind.end()){
+			kind_num[find(cloth_kind.begin(), cloth_kind.end(), clothes[i][1])] +=1;
+		}
+		else{
+			cloth_kind.push_back(clothes[i][1]);
+			kind_num[find(cloth_kind.begin(), cloth_kind.end(), clothes[i][1])] +=1;
+		}
+	} 
+    vector<int>::size_type len2 = kind_num.size();
+    
+    for(int i=1; i<=len2; ++i){
+    	
+	}
+    
+    
+    return answer;
+}
--- cpp/testCode.cpp
+++ cpp/testCode.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
  
 using namespace std;
 
@@ -14,10 +15,28 @@ int main(){
 	cout << len << endl;
 	*/
 	/*모든 원소가 같은 크기의 int 타입인 벡터에서 index를 입력하면 어떻게 되나*/
-	vector<int> same(5, 7);
+	/*vector<int> same(5, 7);
 	cout << same[1] << endl;
 	for(int i=0; i!=5; ++i){
 		cout << same[i] << ends;
 	}
+	*/
+	/*이차원 vector에서 size() 함수를 어떻게 써야 하는가 -> 열을 출력한다*/
+	int x =0;
+	int y =0;
+	vector<int> first;
+	vector<vector<int> > second;
+	while(cin >> x){
+		first.push_back(x);
+		cin >> y; 
+		first.push_back(y);
+	}
+	int z =0;
+	while(z < 10){
+		second.push_back(first);
+		++z;
+	}
+	cout << first.size() << " " << second.size() << endl;
+	 
 	return 0;
 }
