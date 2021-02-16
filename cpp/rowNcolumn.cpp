a34e1d9073d396016fe655fa97bfae8568c2c363
finejy <yoon2926@khu.ac.kr>
2021-02-17 01:25:47 +0900

    Complete question, but find new question about it.

--- cpp/rowNcolumn.cpp
+++ cpp/rowNcolumn.cpp
@@ -8,14 +8,15 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
     
     
     vector<vector<int>>::size_type row_1 = arr1.size();
-    vector<int>::size_type column = arr1[0].size();
+    vector<int>::size_type column_1 = arr1[0].size();
+    vector<int>::size_type column_2 = arr2[0].size();
     //vector<vector<int>>::size_type row_2 = arr2.size();
     
     for(int i=0; i!=row_1; ++i){
     	vector<int> answer_element;
-    	for(int j=0; j!=column; ++j){
+    	for(int j=0; j!=column_2; ++j){
     		int element = 0;
-    		for(int k=0; k!=column; ++k){
+    		for(int k=0; k!=column_1; ++k){
     			element+=arr1[i][k]*arr2[k][j];
 			}
 			answer_element.push_back(element);
@@ -23,6 +24,9 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 		answer.push_back(answer_element);
 		
 	}
-
+    
+    
     return answer;
 }
+
+//왜 17행의 for문의 범위가 column_2까지여야 하는가. column 1과 2의 차이는? 
