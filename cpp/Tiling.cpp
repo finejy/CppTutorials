8634aac43a9d1e078fe5ec7539ce467b61ec4f36
finejy <yoon2926@khu.ac.kr>
2021-02-24 23:49:07 +0900

    Continue solve this problem, but I think this code make some null data and make different answer.

--- cpp/Tiling.cpp
+++ cpp/Tiling.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-/*
+
 long long solution(int n){
 	long long answer = 0;
 	long long each_case = 1;
@@ -23,19 +23,24 @@ long long solution(int n){
 	//cout << answer<< endl;
 	
 	return answer % 1000000007;
-}*/
+}
 
-//solution2. 정직하게 계산해보자.
+//solution2. 정직하게 계산해보자."floating point exception 에러 발생. 아마 long long  이상의 값을 연산하면서 음수값, 0을 나눗셈 수식에 대입하면서 발생하는 듯 하다.
+//+ 시간 초과 에러 발생.  
+/*
 
-long long calc(int n){
-	long long result = 1;
-	if(n==0) return 1;
-	else{
-		for(int i=1; i!=n+1; ++i){
-			result*=i;
-		}
+
+long long calc(int a, int b){
+	int maximum = max(a, b);
+	int minimum = min(a, b);
+	long long result_1 = 1;
+	long long result_2 = 1;
+	long long result = 0;
+	for(int i=maximum+1; i!=maximum+minimum+1; ++i){
+		result_1*=i;
+		result_2*=i-maximum;
 	}
-	cout << result << endl;
+	result = result_1/result_2;
 	
 	return result;
 }
@@ -48,7 +53,7 @@ long long solution(int n){
 	for(int i=0; i!=case_num+1; ++i){
 		int j = n - i*2;
 		
-		long long calculate = calc(i+j)/(calc(i)*calc(j));
+		long long calculate = calc(i, j);
 		
 		answer+=calculate;
 		//cout << answer << endl;
@@ -56,9 +61,7 @@ long long solution(int n){
 	
 	return answer;
 	
-	
-	
-} 
+} */
 
 
 int main(){
