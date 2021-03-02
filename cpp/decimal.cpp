c63880b96ab9c823d85ff134a07febbd67dcf4ba
finejy <yoon2926@khu.ac.kr>
2021-03-02 21:55:35 +0900

    Upgrade code with using list  and iterator.

--- cpp/decimal.cpp
+++ cpp/decimal.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <list>
 
 using namespace std;
 /*
@@ -35,12 +36,15 @@ int solution(int n) {
 	
     return answer;
 }*/
-
+/*list, iterator »ç¿ë ver.1 
 int solution(int n) {
     int answer = 0;
-    vector<int> num={2, 3};
     if(n==2) return 1;
     else if(n==3) return 2;
+    
+    list<int> num;
+    num.push_back(2);
+    num.push_back(3);
     
 	for(int i=1; i!=n-1; ++i){
     	if((i+2)%2!=0&&(i+2)%3!=0){
@@ -48,24 +52,51 @@ int solution(int n) {
 		}
 	}
 	
+	cout << num.size() << endl;
+	
+	list<int>::iterator iter_1 = num.begin();
+	++iter_1;
+	++iter_1;
+	list<int>::iterator iter_2 = ++iter_1;
+	
+	while(*iter_2 <= n/2){
+		
+		if(*iter_2%*iter_1==0){
+			iter_2 = num.erase(iter_2);
+		}
+		else{
+			++iter_2;
+		}
+		if(iter_2==num.end()){
+			++iter_1;
+			iter_2 = ++iter_1;
+		}
+		
+	}
+	
+	answer = num.size();
+	
+	cout << answer << endl;
+	
+    return answer;
+}*/
+/*
+int solution(int n) {
+    vector<int> num={2, 3};
+    if(n==2) return 1;
+    else if(n==3) return 2;
+    
+	for(int i=1; i!=n-1; ++i){
+    	if((i+2)%2!=0){
+            if((i+2)%3!=0){
+    		num.push_back(i+2);
+            }
+		}
+	}
+	
 	int index_1=2;
 	vector<int>::size_type len = num.size();
-	/*
-	while(num[index_1] <= n/2){
-		int index_2=index_1+1;
-		while(index_2 < len){
-			if(num[index_2]%num[index_1]==0){
-				num.erase(num.begin()+index_2);
-				--len;
-			}
-			else{
-				++index_2;
-			}
-		}
-		++index_1;
-		
-	}*/
-	
+
 	int index_2 = 3;
 	while(num[index_1] <= n/2){
 		
@@ -80,11 +111,51 @@ int solution(int n) {
 			++index_1;
 			index_2 = index_1+1;
 		}
-		
+	}
+	cout << len << endl;
+	
+    return len;
+}*/
+
+int solution(int n) {
+    list<int> num;
+    num.push_back(2);
+    num.push_back(3);
+    
+    if(n==2) return 1;
+    else if(n==3) return 2;
+    
+	for(int i=1; i!=n-1; ++i){
+    	if((i+2)%2!=0){
+            if((i+2)%3!=0){
+    		num.push_back(i+2);
+            }
+		}
 	}
 	
+	list<int>::iterator iter_1 = num.begin();
+	++iter_1;
+	++iter_1;
+	list<int>::iterator iter_2 = iter_1;
+	++iter_2;
+	while(*iter_1 <= n/2){
+		list<int>::size_type len = num.size();
+		if((*iter_2)%(*iter_1)==0){
+			iter_2 = num.erase(iter_2);
+			--len;
+		}
+		else{
+			++iter_2;
+			--len;
+		}
+		if(len == 0){
+			++iter_1;
+			iter_2 = iter_1;
+			++iter_2;
+		}
+	}
+	int answer = num.size();
 	cout << answer << endl;
-	answer = len;
 	
     return answer;
 }
